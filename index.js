f10dd9ddb9818622ada7faff1d5611c4a81ea112
Sanidhya Mahajan <sanidhya10628@gmail.com>
2021-06-22 17:27:15 +0530

    update done

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose')
 const Profile = require('./models/profile')
+const methodOverride = require('method-override')
+
+
+
 
 mongoose.connect('mongodb://localhost:27017/CRUD', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("Connection is on!....")
@@ -17,25 +21,58 @@ app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }))
-
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home');
 })
+
+
+app.get('/students', async (req, res) => {
+    const profiles = await Profile.find({});
+    //console.log(profiles);
+    res.render('students', { profiles });
+})
+
+app.get('/students/:id', async (req, res) => {
+    const { id } = req.params;
+    const profile = await Profile.findById(id);
+    //console.log(profile)
+    res.render('display/send', { profile });
+})
+
+
+
 
 app.get('/add', (req, res) => {
     res.render('Methods/add');
 })
 
 app.post('/add', async (req, res) => {
-    const newpro = new Profile(req.body);
-    await newpro.save();
-    res.redirect('/');
+    const newprofile = new Profile(req.body);
+    await newprofile.save();
+    const profile = newprofile;
+    res.render('display/send', { profile });
 })
 
-app.get('/update', (req, res) => {
-    res.render('Methods/update');
+app.get('/students/:id/update', async (req, res) => {
+    // console.log(req.params);
+    const { id } = req.params;
+    const profile = await Profile.findById(id);
+    // console.log(profile);
+    res.render('Methods/update', { profile });
 })
+
+app.put('/students/:id', async (req, res) => {
+    console.log(req.body);
+    const { id } = req.params;
+    const profile = await Profile.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    res.redirect(`/students/${profile._id}`)
+})
+
+
+
+
 
 app.get('/delete', (req, res) => {
     res.render('Methods/delete');
@@ -48,3 +85,6 @@ app.get('/find', (req, res) => {
 app.listen(8000, () => {
     console.log("ON PORT: 8000")
 })
+
+
+
